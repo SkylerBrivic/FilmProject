9d37339bf51dca55f076b4fac3490bd5cc2a95cc
SkylerBrivic <48463701+SkylerBrivic@users.noreply.github.com>
2019-04-22 22:14:12 -0400

    Update AdministratorDataView.java

--- Src/AdministratorDataView.java
+++ Src/AdministratorDataView.java
@@ -1,3 +1,6 @@
+package filmProjectServlets;
+import filmObjects.*;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -7,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
-
-import java.sql.*;
 
 
 @WebServlet("/AdministratorDataView")
@@ -40,16 +41,9 @@ public class AdministratorDataView extends HttpServlet {
 	//and the rest of the string after that contains either 1 (for forward sort) or -1 (for backwards sort)
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		String serverName = "localhost:3306";
-		String databaseName = "FilmProject";
-		String userName = "root";
-		String password = "PondFish";
-		String productTable = "ProductList";
-		String checkoutTable = "checkoutList";
-		String studentTable = "studentList";
+	
 		
 		int filterStatus = Integer.parseInt(request.getParameter("filterStatus"));
-		
 		String TransactionNumber = request.getParameter("TransactionNumber");
 		String ProductNumber = request.getParameter("ProductNumber");
 		String Manufacturer = request.getParameter("manufacturer");
@@ -60,126 +54,47 @@ public class AdministratorDataView extends HttpServlet {
 		String Email = request.getParameter("email");
 		
 		
-		String finalTransactionNum = "", finalProductNum = "", finalManufacturer = "", finalProduct = "", finalStudentName = "", finalStudentNum = "", finalOrganizationName = "", finalEmail = "", finalCheckinDate = "", finalCheckoutDate = "", myQuery = "";
 		
 		String userPassword = request.getParameter("password");
 		String sortOrder = request.getParameter("sortOrder");
 		ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
 		KeywordMatcher keywordMatcher = new KeywordMatcher();
-		PasswordValidator validator = new PasswordValidator();
+		DatabaseInterface databaseInterface = new DatabaseInterface();
 		TransactionComparator transactionComparator = new TransactionComparator();
 		
-		if(validator.validate(userPassword) == false)
+		if(databaseInterface.validatePassword(userPassword) == false)
 		{
 			response.getWriter().println("2");
 			return;
 		}
 		
 		
-		
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			}
-			
-			catch(ClassNotFoundException e)
-			{	
-			}
-			
-		try(Connection connection = DriverManager.getConnection("jdbc:mysql://" + serverName + "/" + databaseName + "?serverTimezone=UTC", userName, password))
-			{
-				Statement statement = connection.createStatement();
-				ResultSet mainResultSet;
-				String transactionNumCriteria, productNumCriteria, manufacturerCriteria, productNameCriteria, studentNameCriteria, studentNumberCriteria, emailCriteria, organizationCriteria, checkoutDateCriteria, checkindateCriteria;
-				 transactionNumCriteria = productNumCriteria = manufacturerCriteria = productNameCriteria = studentNameCriteria = studentNumberCriteria = emailCriteria = organizationCriteria = checkoutDateCriteria =  checkindateCriteria = "";
-
-				 if(keywordMatcher.isEmpty(TransactionNumber) && keywordMatcher.isEmpty(ProductNumber) && keywordMatcher.isEmpty(StudentNumber))
-					 myQuery = "Select * from " + checkoutTable + ";";
-				
-				 else
-					 {
-				 if(!keywordMatcher.isEmpty(TransactionNumber))
-					 
-					transactionNumCriteria = " TransactionNumber = " + TransactionNumber + " AND"; 
-				if(!keywordMatcher.isEmpty(ProductNumber))
-					productNumCriteria = " QR_Code = " + ProductNumber + " AND";
-				if(!keywordMatcher.isEmpty(StudentNumber))
-						studentNumberCriteria = " StudentNumber = '" + StudentNumber + "' AND";
-			
-				myQuery = "SELECT * FROM " + checkoutTable + " WHERE " + transactionNumCriteria + productNumCriteria + studentNumberCriteria;
-				int indexOfLastAnd = myQuery.lastIndexOf("AND");
-				
-				if(indexOfLastAnd != -1)
-				{
-					if(indexOfLastAnd >= myQuery.length() - 3)
-						myQuery = myQuery.substring(0, indexOfLastAnd);
-				}
-				myQuery += ";";
-					 }
-				 
-				mainResultSet = statement.executeQuery(myQuery);	
-				
-					
-				
-				while(mainResultSet.next())
-				{
-					if(filterStatus == 0 || (filterStatus == 1 && mainResultSet.getString(5) != null) || (filterStatus == 2 && mainResultSet.getString(5) == null))
-					{
-						
-						finalTransactionNum = mainResultSet.getString(1);
-						finalProductNum = mainResultSet.getString(2);
-						finalStudentNum = mainResultSet.getString(3);
-						Statement otherStatement = connection.createStatement();
-						ResultSet tempResultSet = otherStatement.executeQuery("SELECT * FROM " + productTable + " WHERE QR_Code = " + finalProductNum + ";");
-						while(tempResultSet.next())
-						{
-						finalManufacturer = tempResultSet.getString(2);
-						finalProduct = tempResultSet.getString(3);
-						}
-						if(keywordMatcher.matchDataStrings(finalManufacturer, Manufacturer) && keywordMatcher.matchDataStrings(finalProduct, ProductName))
-						{
-							Statement thirdStatement = connection.createStatement();
-							ResultSet thirdResultSet = thirdStatement.executeQuery("SELECT * FROM " + studentTable + " WHERE StudentNumber = '" + finalStudentNum + "';");
-							
-							if(thirdResultSet.next())
-							{
-								finalStudentName = thirdResultSet.getString(2);
-							if(!keywordMatcher.isEmpty(StudentName) && !keywordMatcher.matchDataStrings(finalStudentName, StudentName))
-								continue;
-								finalOrganizationName = thirdResultSet.getString(3);
-							if(!keywordMatcher.isEmpty(OrganizationName) && !keywordMatcher.matchDataStrings(finalOrganizationName, OrganizationName))
-								continue;
-								finalEmail = thirdResultSet.getString(4);
-							if(!keywordMatcher.isEmpty(Email) && !keywordMatcher.matchDataStrings(finalEmail, Email))
-								continue;	
-							}
-							finalCheckoutDate = mainResultSet.getString(4);
-							finalCheckinDate = mainResultSet.getString(5);
-							if(finalCheckinDate == null)
-								finalCheckinDate = "N/A";
-							if(finalStudentName == null)
-								finalStudentName = "N/A";
-							if(finalOrganizationName == null)
-								finalOrganizationName = "N/A";
-							if(finalEmail == null)
-								finalEmail = "N/A";
-								
-							transactionList.add(new Transaction(finalTransactionNum, finalProductNum, finalManufacturer, finalProduct, finalStudentNum, finalStudentName, finalEmail, finalOrganizationName, finalCheckoutDate, finalCheckinDate));
-							
-
-						}
-						
-					}
-					
-					
-				}
-			}
-			catch(SQLException e)
-			{
-				e.printStackTrace();
-			}
-			
 			char sortCriteria = sortOrder.charAt(0);
+			
+			ArrayList<Transaction>  allTransactions = databaseInterface.selectTransaction(filterStatus);
+			
+			for(int i = 0; i < allTransactions.size(); ++i)
+			{
+				if(TransactionNumber != null && !TransactionNumber.equals("") && !TransactionNumber.equals(allTransactions.get(i).transactionNumber))
+					continue;
+				if(ProductNumber != null && !ProductNumber.equals("") && !ProductNumber.equals(allTransactions.get(i).QR_Code))
+					continue;
+				if(Manufacturer != null && !Manufacturer.equals("") && !keywordMatcher.matchDataStrings(allTransactions.get(i).manufacturerName, Manufacturer))
+					continue;
+				if(ProductName != null && !ProductName.equals("") && !keywordMatcher.matchDataStrings(allTransactions.get(i).productName, ProductName))
+					continue;
+				if(StudentName != null && !StudentName.equals("") && !StudentName.equalsIgnoreCase(allTransactions.get(i).studentName))
+					continue;
+				if(StudentNumber != null && !StudentNumber.equals("") && !StudentNumber.equalsIgnoreCase(allTransactions.get(i).studentNumber))
+					continue;
+				if(OrganizationName != null && !OrganizationName.equals("") && !keywordMatcher.matchDataStrings(allTransactions.get(i).organizationName, OrganizationName))
+					continue;
+				if(Email != null && !Email.equals("") && !Email.equals(allTransactions.get(i).studentEmail))
+					continue;
+				
+				transactionList.add(allTransactions.get(i));
+			}
+			
 			
 		if(Integer.parseInt(sortOrder.substring(1)) == 1)
 		{
