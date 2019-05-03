12e30e2f7b472f3dad5774f09a3eae27673b7028
SkylerBrivic <48463701+SkylerBrivic@users.noreply.github.com>
2019-05-03 03:02:10 -0400

    Update UpdateProduct.java

--- Src/filmProjectServlets/UpdateProduct.java
+++ Src/filmProjectServlets/UpdateProduct.java
@@ -31,42 +31,57 @@ public class UpdateProduct extends HttpServlet {
 	//the parameter QR_Code contains the QR Code of the product to be updated
 	//the parameter manufacturer contains the new manufacturer name of the product
 	//the parameter product contains the new name for the product
-	//if manufacturer or product name is null or consists only of white space (if isEmpty returns true) then that particulular attribute is not updated for the product
+	//if manufacturer or product name is null or consists only of white space (if isEmpty returns true) then that particular attribute is not updated for the product
 	
 	//a return value of 0 means the product's information was successfully updated
 	//a return value of 1 means that the product's QR Code was invalid.
-	//a return value of 2 means that the user entered an invalid password, and needs to login again.
+	//a return value of 2 means that the user entered in an invalid password, and needs to login again.
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{	
-	System.out.println("In UpdateProduct servlet");
 	String QR_Code = request.getParameter("QR_Code");
 	String manufacturer = request.getParameter("manufacturer");
 	String product = request.getParameter("product");
 	String userPassword = request.getParameter("password");
+	String conditionList = "";
 	DatabaseInterface databaseInterface = new DatabaseInterface();
+	KeywordMatcher keywordMatcher = new KeywordMatcher();
 	if(databaseInterface.validatePassword(userPassword) == false)
 	{
 		response.getWriter().println("2");
 		return;
 	}
+	
+	if(keywordMatcher.isEmpty(QR_Code))
+	{
+		response.getWriter().println("1");
+		return;
+	}
+	
 	ArrayList<Product> productName = databaseInterface.selectProduct(" WHERE QR_Code = " + QR_Code);
 	if(productName.size() == 0)
 	{
 		response.getWriter().println("1");
 		return;
 	}
+	//quoting any single quotes that may be in the product name or manufacturer name so that they can be inserted into the database
 	CharSequence target = (String)"\'";
 	CharSequence replacement = (String)"\'\'";
-	try
-	{
-		manufacturer = manufacturer.replace(target, replacement);
-	}
-	catch(NullPointerException e)
-	{
-	e.printStackTrace();
-	}
+	
+	manufacturer = manufacturer.replace(target, replacement);
 	product = product.replace(target, replacement);
-	databaseInterface.updateProduct(QR_Code, manufacturer, product);
+	if((manufacturer == null || keywordMatcher.isEmpty(manufacturer)) && (product == null  || keywordMatcher.isEmpty(product)))
+	{
+		response.getWriter().println("0");
+		return;
+	}
+	else if(manufacturer == null || keywordMatcher.isEmpty(manufacturer))
+		conditionList = " productName = '" + product + "' ";
+	else if(product == null || keywordMatcher.isEmpty(product))
+		conditionList = " manufacturer = '" + manufacturer + "' ";
+	else
+		conditionList = " productName = '" + product + "', manufacturer = '" + manufacturer + "' ";
+	
+	databaseInterface.updateProduct(QR_Code, conditionList);
 	response.getWriter().println("0");
 }
 }
