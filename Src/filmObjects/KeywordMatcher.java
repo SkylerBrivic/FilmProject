ca8c03b469610dc9e979d4794be4ec450b95b889
SkylerBrivic <48463701+SkylerBrivic@users.noreply.github.com>
2019-05-03 02:49:16 -0400

    Update KeywordMatcher.java

--- Src/filmObjects/KeywordMatcher.java
+++ Src/filmObjects/KeywordMatcher.java
@@ -3,6 +3,9 @@ package filmObjects;
 
 public class KeywordMatcher 
 {
+	//matchDataStrings returns true if smallerString is the empty String or has only whitespace characters in it.
+	//Additionally, if longString contains all the characters of smallerString consecutively at any point, then true is returned.
+	//Otherwise, false is returned (ex. matchDataStrings("television", "vis") returns true, while matchDataStrings("television", "vid") returns false)
 	 public boolean matchDataStrings(String longString, String smallerString)
 	    {
 	    	int longStringIndex = 0, smallStringIndex = 0;
@@ -35,7 +38,8 @@ public class KeywordMatcher
 	    	return false;
 	    	
 	    }
-	 
+	 //isEmpty returns true if inputString is null, is the empty string, or contains only whitespace characters
+	 //otherwise, it returns false.
 		public boolean isEmpty(String inputString)
 		{
 			if(inputString == null || inputString.equals(""))
