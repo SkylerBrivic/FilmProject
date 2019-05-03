d46aca33bec4d15d45ac1afb68c9beaa39ec909c
SkylerBrivic <48463701+SkylerBrivic@users.noreply.github.com>
2019-05-03 02:52:03 -0400

    Update Transaction.java

--- Src/filmObjects/Transaction.java
+++ Src/filmObjects/Transaction.java
@@ -1,12 +1,14 @@
 package filmObjects;
 
 
+//a Transaction object stores the transaction number of a transaction, the QR Code, manufacturer name and product name of the product associated with that transaction,
+//the student number, student name, email address, and organization of the student who checked the product out, and the checkout date, checkin date, and expected checkin date of the product
 public class Transaction 
 {
 	
-public String transactionNumber, QR_Code, manufacturerName, productName, studentNumber, studentName, studentEmail, organizationName, checkoutDate, checkinDate;
+public String transactionNumber, QR_Code, manufacturerName, productName, studentNumber, studentName, studentEmail, organizationName, checkoutDate, checkinDate, expectedCheckinDate;
 
-	public Transaction(String newTransaction, String newQR, String newManufacturer, String newProduct, String newNumber, String newName, String newEmail, String newOrganization, String newCheckout, String newCheckin)
+	public Transaction(String newTransaction, String newQR, String newManufacturer, String newProduct, String newNumber, String newName, String newEmail, String newOrganization, String newCheckout, String newCheckin, String newExpectedDate)
 	{
 		transactionNumber = newTransaction;
 		QR_Code = newQR;
@@ -18,6 +20,7 @@ public String transactionNumber, QR_Code, manufacturerName, productName, student
 		organizationName = newOrganization;
 		checkoutDate = newCheckout;
 		checkinDate = newCheckin;
+		expectedCheckinDate = newExpectedDate;
 	}
 
 }
