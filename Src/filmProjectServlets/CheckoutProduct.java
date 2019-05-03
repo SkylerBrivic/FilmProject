4153fd07fa25df626e99ff7a0e9b9db69e15d0de
SkylerBrivic <48463701+SkylerBrivic@users.noreply.github.com>
2019-05-03 03:00:11 -0400

    Update CheckoutProduct.java

--- Src/filmProjectServlets/CheckoutProduct.java
+++ Src/filmProjectServlets/CheckoutProduct.java
@@ -2,6 +2,7 @@ package filmProjectServlets;
 import filmObjects.*;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,8 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-import java.util.ArrayList;
+import java.text.*;
+import java.util.*;
 
 
 @WebServlet("/CheckoutProduct")
@@ -32,6 +33,14 @@ public class CheckoutProduct extends HttpServlet {
 	//a return value of 1 means the specified QR Code was invalid
 	//a return value of 2 means that the product is already checked out by somebody else
 	//a return value of 3 means the user does not have permission to check this product out (due to an invalid password)
+	//a return value of 4 means that studentName was not filled in
+	//a return value of 5 means that the organizationName was not filled in
+	//a return value of 6 means that an expected return date was not filled in
+	//a return value of 7 means that the date was not in yyyy-MM-dd format
+	//a return value of 8 means that the studentNumber was not filled in
+	//the parameter QR_Code stores the products QR Code, the parameter StudentNumber stores the student's number, the parameter OrganizationName
+	//stores the organization's name, the parameter Email stores the email of the person checking the product out, password stores the password for the
+	//website, and ExpectedReturnDate stores the product's expected return date in yyyy-MM-dd format.
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{		
 		String QR_Code = request.getParameter("QR_Code");
@@ -40,8 +49,14 @@ public class CheckoutProduct extends HttpServlet {
 		String organizationName = request.getParameter("OrganizationName");
 		String email = request.getParameter("Email");
 		String userPassword = request.getParameter("password");
+		String userDate = request.getParameter("ExpectedReturnDate");
 		KeywordMatcher keywordMatcher = new KeywordMatcher();
 		DatabaseInterface databaseInterface = new DatabaseInterface();
+		if(keywordMatcher.isEmpty(QR_Code))
+		{
+			response.getWriter().println("1");
+			return;
+		}
 		if(databaseInterface.validatePassword(userPassword) == false)
 		{
 			response.getWriter().println("3");
@@ -53,21 +68,61 @@ public class CheckoutProduct extends HttpServlet {
 			response.getWriter().println("1");
 			return;
 		}
-		if(!productCheck.get(0).checkoutDate.equals("N/A"))
+		if(!productCheck.get(0).checkoutDate.equalsIgnoreCase("N/A"))
 		{
 			response.getWriter().println("2");
 			return;
 		}
-		if(studentNumber == null || keywordMatcher.isEmpty(studentNumber))
-			studentNumber = "N/A";
+		
 		if(studentName == null || keywordMatcher.isEmpty(studentName))
-			studentName = "N/A";
+		{
+			response.getWriter().println("4");
+			return;
+		}
 		if(organizationName == null || keywordMatcher.isEmpty(organizationName))
-			organizationName = "N/A";
+		{
+			response.getWriter().println("5");
+			return;
+		}
+		if(userDate == null || keywordMatcher.isEmpty(userDate))
+		{
+			response.getWriter().println("6");
+			return;
+		}
+		else
+		{
+			try
+			{
+				@SuppressWarnings("unused")
+				DateFormat firstDate = new SimpleDateFormat("yyyy-MM-dd");
+				userDate = firstDate.format(firstDate.parse(userDate));
+		
+			}
+			catch(ParseException e)
+			{
+				response.getWriter().println("7");
+				return;
+			}
+		}
+		
+		if(studentNumber == null || keywordMatcher.isEmpty(studentNumber))
+			{
+			response.getWriter().println("8");
+			return;
+			}
+		
 		if(email == null || keywordMatcher.isEmpty(email))
 			email = "N/A";
 		
-		databaseInterface.checkoutProduct(QR_Code, studentNumber, studentName, organizationName, email);
+		CharSequence target = "\'";
+		CharSequence replacement = "\'\'";
+		QR_Code = QR_Code.replace(target, replacement);
+		studentNumber = studentNumber.replace(target, replacement);
+		studentName = studentName.replace(target, replacement);
+		organizationName = organizationName.replace(target, replacement);
+		email = email.replace(target, replacement);
+		
+		databaseInterface.checkoutProduct(QR_Code, studentNumber, studentName, organizationName, email, userDate);
 		response.getWriter().println("0");
 	}
 }
