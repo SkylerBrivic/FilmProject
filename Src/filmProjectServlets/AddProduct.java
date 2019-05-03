b78d268a1d69fd4960236489857935c7c212eb90
SkylerBrivic <48463701+SkylerBrivic@users.noreply.github.com>
2019-05-03 02:54:10 -0400

    Update AddProduct.java

--- Src/filmProjectServlets/AddProduct.java
+++ Src/filmProjectServlets/AddProduct.java
@@ -30,9 +30,10 @@ public class AddProduct extends HttpServlet {
 	
 	//the parameter manufacturer stores the manufacturer's name
 	//the parameter productName stores the product's name
-	//a value of "1" for isAvailable means the product is available for rental, while a value of "0" means the product is checked out
-	//if the user entered in a valid password earlier to log in, then the function returns 0. Otherwise, it returns 1 and the user's
-	//request is denied
+	//the parameter quantity stores the number of items to insert into the database
+	//if the user entered in a valid password earlier to log in and a valid quantity, then the function returns 0. Otherwise, if the password was wrong, it returns 1 and the user's
+	//request is denied. If the user entered in an invalid quantity (ie. <= 0 or not an int) then 2 is returned.
+	//if the user did not enter in a product name or manufacturer name, then 3 is returned and the product is not added to the database.
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
@@ -40,8 +41,31 @@ public class AddProduct extends HttpServlet {
 		
 		String manufacturer = request.getParameter("manufacturer");
 		String product = request.getParameter("productName");
+		String quantity = request.getParameter("quantity");
 		String userPassword = request.getParameter("password");
+		KeywordMatcher keywordMatcher = new KeywordMatcher();
+		int numProducts = 0;
+		try
+		{
+			numProducts = Integer.parseInt(quantity);
+			if(numProducts <= 0)
+			{
+				response.getWriter().println("2");
+				return;
+			}
+		}
 		
+		catch(NumberFormatException e)
+		{
+			response.getWriter().println("2");
+			return;
+		}
+		
+		if(keywordMatcher.isEmpty(manufacturer) || keywordMatcher.isEmpty(product))
+		{
+			response.getWriter().println("3");
+			return;
+		}
 		DatabaseInterface databaseInterface = new DatabaseInterface();
 		
 		if(databaseInterface.validatePassword(userPassword) == false)
@@ -50,14 +74,13 @@ public class AddProduct extends HttpServlet {
 			return;
 		}
 		
+		//quoting single quotes so that they can appear in the product or manufacturer name
 		CharSequence target = "\'";
 		CharSequence replacement = "\'\'";
 		manufacturer = manufacturer.replace(target, replacement);
 		product = product.replace(target, replacement);
-		databaseInterface.addProduct(manufacturer,  product);
+		databaseInterface.addProduct(manufacturer,  product, numProducts);
 		response.getWriter().println("0");
 		return;
 		}		
 	}
-
-
