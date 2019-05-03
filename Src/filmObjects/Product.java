fe1ccbc06bd5989498929948ce7e9dc350fe935b
SkylerBrivic <48463701+SkylerBrivic@users.noreply.github.com>
2019-05-03 02:49:45 -0400

    Update Product.java

--- Src/filmObjects/Product.java
+++ Src/filmObjects/Product.java
@@ -1,6 +1,7 @@
 package filmObjects;
 
-
+//a Product object represents the product ID number of a product, the manufacturer name of a product, the product name of the product,
+//the checkoutDate of the product (if it is currently checked out), and the checkinDate of the product (both date Strings are in yyyy-MM-dd format).
 public class Product {
 	
 	public String QR_Code, ManufacturerName, ProductName, checkoutDate, checkinDate;
